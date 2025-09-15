e5a6291eb6c8055167211fc48fdc73ec6cb42732
Govinda Kumar <kumargovinda08@gmail.com>
2025-09-15 23:59:43 +0530

    Adding Assignment1

---
+++ Assignment1/Task2.py
@@ -0,0 +1,4 @@
+fName = input("Enter your first name: ")
+lName = input("Enter your last name: ")
+print()
+print("Hello " + fName.strip() + " " + lName.strip() + "! Welcome to Python Program.")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# LearningPython
+# Learning Python
 Assignments for Python via tutedude
+
+### [Assignment 1](Assignment1)
